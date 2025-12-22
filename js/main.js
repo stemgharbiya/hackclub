f8d137d1beb85365b92b1df9ab219dbd00c72c8b
Amr Loksha <amrloksha151@proton.me>
2025-12-22 16:56:20 +0200

    last modifications until upcoming iteration

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
-// Resizing Heading when scrolling --px down
+// Resizing Heading and Flag when scrolling
 let header = document.querySelector('.header');
+let flag = document.querySelector('img[src*="flag-orpheus-top"]');
 
-window.onscroll = function() {
+window.onscroll = function () {
     if (window.scrollY > 10) {
-        header.classList.add('scrolled');  
+        header.classList.add('scrolled');
+        if (flag) flag.classList.add('scrolled');
     } else {
         header.classList.remove('scrolled');
+        if (flag) flag.classList.remove('scrolled');
     }
 };
 
