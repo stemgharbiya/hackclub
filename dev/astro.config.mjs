97df4f673841487d2d1967c9bf72715b252f80f5
Amr Loksha <amrloksha151@proton.me>
2026-03-08 17:37:57 +0200

    replacing hard coded icons

--- dev/astro.config.mjs
+++ dev/astro.config.mjs
@@ -2,9 +2,10 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://gharbiya.hackclub.com',
-	integrations: [tailwind(), sitemap()],
+	integrations: [tailwind(), sitemap(), react()],
 });
